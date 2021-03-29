da87c239a07818baca61e1bd2e8ca9c9ef5d550e
Duy Dien <duydien17031999@gmail.com>
2021-03-30 00:14:06 +0700

    update CNN_Model

--- CNN_Model.py
+++ CNN_Model.py
@@ -1,10 +1,10 @@
 from _ast import mod
-
 from keras.activations import sigmoid
 from tensorflow import keras
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from keras_applications.nasnet import models
+import numpy as np
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -21,7 +21,15 @@ model.compile(optimizer='adam' ,loss = 'sparse_categorical_crossentropy', metric
 
 model.fit(x_train, y_train, epochs=3)
 
+val_loss, val_acc = model.evaluate(x_test, y_test)
+print(val_loss, val_acc)
 
-# plt.imshow(x_train[1], cmap = plt.cm.binary)
-# plt.show()
-# print(x_train[1])
+model.save('Model/my_model.h5')
+new_model = tf.keras.models.load_model('Model/my_model.h5')
+predictions = new_model.predict([x_test])
+print(np.argmax(predictions[0]))
+
+plt.imshow(x_test[0])
+plt.show()
+
+
